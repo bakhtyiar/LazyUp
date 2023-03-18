bdb968815df555d1ef4ff13eaeab9e355c1bb749
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-03-18 20:36:54 +0300

    Add project files.

---
+++ LazyUp/LockScreen.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Timers;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace LazyUp
+{
+    /// <summary>
+    /// Interaction logic for LockScreen.xaml
+    /// </summary>
+    public partial class LockScreen : Window
+    {
+        string lockScreenHeader = ConfigurationManager.AppSettings["lockScreenHeader"];
+        string lockScreenParagraph = ConfigurationManager.AppSettings["lockScreenParagraph"];
+        string lockScreenBackgroundColor = ConfigurationManager.AppSettings["lockScreenBackgroundColor"];
+        string durationBreakSec = ConfigurationManager.AppSettings["durationBreakSec"];
+        string strictBreaks = ConfigurationManager.AppSettings["strictBreaks"];
+
+        private int breakSecsLast;
+        private int timeIntervalSec;
+        private Timer aTimer;
+
+        private delegate void TimeLastOutput();
+        private void SetTimerText(TextBlock textBlock, ref int secsLast)
+        {
+            int hours = secsLast / 60 / 60;
+            int minutes = (secsLast / 60) - (60 * hours);
+            int seconds = (secsLast) - (60 * minutes) - (60 * 60 * hours);
+            string text = "";
+            if (hours > 0)
+            {
+                text += Convert.ToString(hours) + " hours ";
+            }
+            if (minutes > 0)
+            {
+                text += Convert.ToString(minutes) + " minutes ";
+            }
+            if (minutes > 0)
+            {
+                text += Convert.ToString(seconds) + " seconds ";
+            }
+            if (hours > 0 || minutes > 0)
+            {
+                text += "left";
+            }
+            textBlock.Dispatcher.BeginInvoke(() => {
+                textBlock.Text = text;
+            });
+        }
+        
+        private delegate void TimelineOutput();
+        private void SetTimelineBarValue(ProgressBar timeline, int value)
+        {
+            timeline.Dispatcher.BeginInvoke(() =>
+            {
+                timeline.Value = value;
+            });
+        }
+
+        public LockScreen()
+        {
+            breakSecsLast = Convert.ToInt32(durationBreakSec);
+            timeIntervalSec = 1;
+            aTimer = new System.Timers.Timer();
+            aTimer.Interval = timeIntervalSec * 1000;
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+            InitializeComponent();
+        }
+
+        private void Header_Initialized(object sender, EventArgs e)
+        {
+            Header.Text = lockScreenHeader;
+        }
+
+        private void Paragraph_Initialized(object sender, EventArgs e)
+        {
+            Paragraph.Text = lockScreenParagraph;
+        }
+
+        private void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            breakSecsLast -= timeIntervalSec;
+            SetTimerText(TimeLast, ref breakSecsLast);
+            int lineLastPercentage = Convert.ToInt32((Convert.ToDouble(breakSecsLast) * 100 / Convert.ToDouble(durationBreakSec)));
+            SetTimelineBarValue(TimelineBar, lineLastPercentage);
+        }
+
+        private void TimeLast_Initialized(object sender, EventArgs e)
+        {
+            int secsLast = Convert.ToInt32(durationBreakSec);
+            SetTimerText(TimeLast, ref secsLast);
+        }
+
+        private void TimelineBar_Initialized(object sender, EventArgs e)
+        {
+            TimelineBar.Value = 100;
+        }
+    }
+}
---
+++ LazyUp/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Reflection;
+using System.Drawing;
+
+namespace LazyUp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string lockScreenHeader = ConfigurationManager.AppSettings["lockScreenHeader"];
+        string lockScreenParagraph = ConfigurationManager.AppSettings["lockScreenParagraph"];
+        string lockScreenBackgroundColor = ConfigurationManager.AppSettings["lockScreenBackgroundColor"];
+        string breaksIntervalSec = ConfigurationManager.AppSettings["breaksIntervalSec"];
+        string durationBreakSec = ConfigurationManager.AppSettings["durationBreakSec"];
+        string strictBreaks = ConfigurationManager.AppSettings["strictBreaks"];
+        string lookHiddenRest = ConfigurationManager.AppSettings["lookHiddenRest"];
+        string startupWithSystem = ConfigurationManager.AppSettings["startupWithSystem"];
+        string startInTray = ConfigurationManager.AppSettings["startInTray"];
+
+        private string getValueTextBox(object sender)
+        {
+            TextBox textBox = (TextBox)sender;
+            string text = textBox.Text;
+            return text;
+        }
+
+        private static void UpdateSetting(string key, string value)
+        {
+            Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            configuration.AppSettings.Settings[key].Value = value;
+            configuration.Save();
+
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RunTestLockScreen_Button_Click(object sender, RoutedEventArgs e)
+        {
+            LockScreen lockWindow = new LockScreen();
+            lockWindow.Show();
+        }
+
+        private void SaveSettings_Button_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateSetting("lockScreenHeader", lockScreenHeader);
+            UpdateSetting("lockScreenParagraph", lockScreenParagraph);
+            UpdateSetting("lockScreenBackgroundColor", lockScreenBackgroundColor);
+            UpdateSetting("breaksIntervalSec", breaksIntervalSec);
+            UpdateSetting("durationBreakSec", durationBreakSec);
+            UpdateSetting("strictBreaks", strictBreaks);
+            UpdateSetting("lookHiddenRest", lookHiddenRest);
+            UpdateSetting("startupWithSystem", startupWithSystem);
+            UpdateSetting("startInTray", startInTray);
+        }
+
+        private void LockScreenHeader_Initialized(object sender, EventArgs e)
+        {
+            LockScreenHeader.Text = lockScreenHeader;
+        }
+
+        private void LockScreenHeaderText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lockScreenHeader = getValueTextBox(sender);
+        }
+
+        private void LockScreenParagraph_Initialized(object sender, EventArgs e)
+        {
+            LockScreenParagraph.Text = lockScreenParagraph;
+        }
+
+        private void LockScreenParagraph_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lockScreenParagraph = getValueTextBox(sender);
+        }
+
+        private void radiobtnThemeDark_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void radiobtnThemeLight_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
