22d31cfe4e53d26655499ba1747140aaa7094245
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-06-06 11:29:05 +0300

    Finally, program that restarts main process without 15% of CPU loading

--- AgentOfLazyUp/Program.cs
+++ AgentOfLazyUp/Program.cs
@@ -1,22 +1,37 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Timers;
-using Timer = System.Threading.Timer;
+using Timer = System.Timers.Timer;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Process process = new Process();
-        Timer timer = new Timer(TimerElapse, null, 0, 1000);
-        void TimerElapse(object? state)
+
+        // Имя программы, которую нужно проверить
+        string processName = "LazyUp";
+
+        void CheckAndStartProcess()
         {
-            Process[] processes = Process.GetProcessesByName("LazyUp");
-            if (processes.Length == 0)
+            while (true)
             {
-                string exePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location).ToString() + @"\LazyUp.exe";
-                Process.Start(exePath);
+                int foundProcesses = Process.GetProcessesByName(processName).Length;
+                // Проверяем, запущена ли программа
+                if (foundProcesses < 1)
+                {
+                    // Программа не запущена, запускаем ее
+                    Process process = Process.Start(processName + ".exe");
+                    process.WaitForExit();
+                }
+
+                // Ждем перед следующей проверкой
+                Thread.Sleep(1000);
             }
         }
+
+        // Запускаем проверку в фоновом потоке
+        Thread thread = new Thread(CheckAndStartProcess);
+        thread.Start();
     }
 }
