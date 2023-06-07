c7ea1c1eaac8989b4d9ea6006c54d1fcb86d2f6b
Bakhtyiar <bakhtiyarkhuramshin@gmail.com>
2023-06-08 01:26:51 +0300

    fixed code style, edited for more readability

--- LazyUp/App.xaml.cs
+++ LazyUp/App.xaml.cs
@@ -1,22 +1,15 @@
-﻿using System;
+﻿using IWshRuntimeLibrary;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-using Forms = System.Windows.Forms;
-using System.Timers;
+using System.Diagnostics;
 using System.IO;
 using System.Reflection;
-using Microsoft.Win32;
-using System.Windows.Forms;
 using System.Runtime.InteropServices;
-using System.Windows.Interop;
-using IWshRuntimeLibrary;
-using System.Diagnostics;
-using Xceed.Wpf.Toolkit;
 using System.Threading;
+using System.Timers;
+using System.Windows;
+using Forms = System.Windows.Forms;
 
 namespace LazyUp
 {
@@ -25,40 +18,61 @@ namespace LazyUp
     /// </summary>
     public partial class App : System.Windows.Application
     {
-        Process process = new Process();
+        readonly Process _process;
 
-        private AppConfigurator configurator = AppConfigurator.GetInstance();
-        private AppSettings config = AppConfigurator.GetInstance().config;
-        string shutdownModeValue;
+        private MainWindow _mainWindow;
 
         private FileSystemWatcher _configWatcher;
-        private System.Timers.Timer timerBreakInterval;
-        private MainWindow mainWindow = new MainWindow();
+        readonly private AppConfigurator _configurator;
+        readonly private AppSettings _config;
+
+        private System.Timers.Timer _timerBreakInterval;
+        string _shutdownModeValue;
+
+        readonly private string _execDirectory;
+        readonly private string _startupFolderPath;
+        readonly private string _programBrandName;
+        readonly private string _programPath;
+        readonly private string _agentReviverProgramPath;
+
         private Forms.NotifyIcon _notifyIcon;
-        private System.Drawing.Icon logoIcon = new System.Drawing.Icon(@"Resources\logo.ico");
-        private System.Drawing.Icon openIcon = new System.Drawing.Icon(@"Resources\open.ico");
-        private System.Drawing.Icon closeIcon = new System.Drawing.Icon(@"Resources\close.ico");
-        private string execDirectory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location).ToString();
-        private string startupFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
-        private string programBrandName = "LazyUp";
-        private string programPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location).ToString() + @"\LazyUp.exe";
-        private static string agentReviverProgramPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location).ToString() + @"\AgentOfLazyUp.exe";
-        private static Thread reviveThread;
+        readonly private System.Drawing.Icon _logoIcon;
+        readonly private System.Drawing.Icon _openIcon;
+        readonly private System.Drawing.Icon _closeIcon;
 
         App()
         {
+            _process = new Process();
+
+            _configurator = AppConfigurator.GetInstance();
+            _config = AppConfigurator.GetInstance().config;
+
+            _mainWindow = new MainWindow();
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                _logoIcon = new System.Drawing.Icon(@"Resources\logo.ico");
+                _openIcon = new System.Drawing.Icon(@"Resources\open.ico");
+                _closeIcon = new System.Drawing.Icon(@"Resources\close.ico");
+            }
+
+            _execDirectory = (Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? @".\").ToString();
+            _startupFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+            _programBrandName = "LazyUp";
+            _programPath = (Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? ".").ToString() + @"\LazyUp.exe";
+            _agentReviverProgramPath = (Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) ?? @".").ToString() + @"\AgentOfLazyUp.exe";
         }
 
         private void OnConfigChanged(object sender, FileSystemEventArgs e)
         {
-            configurator.RefreshConfig();
-            SetStartupProgram(config.StartupWithSystem);
-            SetProgramVisibility(config.HideProgram);
-            SetCloseInTray(config.CloseInTray);
-            ReviveAgentOfLazyUp(config.ReviveProgram);
+            _configurator.RefreshConfig();
+            SetStartupProgram(_config.StartupWithSystem);
+            SetProgramVisibility(ref _notifyIcon, _config.HideProgram);
+            SetCloseInTray(out _shutdownModeValue, _config.CloseInTray);
+            ReviveAgentOfLazyUp(_config.ReviveProgram);
         }
 
-        private void SetCloseInTray(bool isCloseInTray)
+        static private void SetCloseInTray(out string shutdownModeValue, bool isCloseInTray)
         {
             shutdownModeValue = isCloseInTray ? "OnExplicitShutdown" : "OnMainWindowClose";
             if (Enum.TryParse<ShutdownMode>(shutdownModeValue, out ShutdownMode shutdownMode))
@@ -71,39 +85,42 @@ namespace LazyUp
             }
         }
 
-        private void SetProgramVisibility(bool isHiddenProgram)
+        static private void SetProgramVisibility(ref Forms.NotifyIcon notifyIcon, bool isHiddenProgram)
         {
-            if (isHiddenProgram)
-            {
-                _notifyIcon.Visible = false;
-            } else
-            {
-                _notifyIcon.Visible = true;
-            }
+            notifyIcon.Visible = !isHiddenProgram;
         }
 
-        private void SetStartupProgram(bool isStartup) {
+        private void SetStartupProgram(bool isStartup)
+        {
             string path = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
             if (isStartup)
             {
-                RegistryKey key = Registry.CurrentUser.OpenSubKey(path, true);
-                key.SetValue(programBrandName, programPath);
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    RegistryKey currentUser = Registry.CurrentUser;
+                    RegistryKey? key = currentUser.OpenSubKey(path, true);
+                    key?.SetValue(_programBrandName, _programPath);
+                }
 
+                string linksPath = Path.Combine(_startupFolderPath, _programBrandName + ".lnk");
                 WshShell shell = new WshShell();
-                IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(Path.Combine(startupFolderPath, programBrandName + ".lnk"));
+                IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(linksPath);
 
-                shortcut.TargetPath = programPath;
-                shortcut.WorkingDirectory = Path.GetDirectoryName(programPath);
-                shortcut.Description = programBrandName;
-                shortcut.IconLocation = Path.Combine(execDirectory, @"Resources\logo.ico");
+                shortcut.TargetPath = _programPath;
+                shortcut.WorkingDirectory = Path.GetDirectoryName(_programPath);
+                shortcut.Description = _programBrandName;
+                shortcut.IconLocation = Path.Combine(_execDirectory, @"Resources\logo.ico");
                 shortcut.Save();
             }
             else
             {
-                RegistryKey key = Registry.CurrentUser.OpenSubKey(path, true);
-                key.DeleteValue(programBrandName, false);
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    RegistryKey? key = Registry.CurrentUser.OpenSubKey(path, true);
+                    key?.DeleteValue(_programBrandName, false);
+                }
 
-                string shortcutPath = Path.Combine(startupFolderPath, programBrandName + ".lnk");
+                string shortcutPath = Path.Combine(_startupFolderPath, _programBrandName + ".lnk");
                 if (System.IO.File.Exists(shortcutPath))
                 {
                     System.IO.File.Delete(shortcutPath);
@@ -111,45 +128,52 @@ namespace LazyUp
             }
         }
 
-        private void NotifyIcon_DoubleClick(object sender, EventArgs e)
+        private void StartBreak(Object? source, System.Timers.ElapsedEventArgs e)
         {
-            mainWindow = mainWindow ?? new MainWindow();
-            mainWindow.WindowState = System.Windows.WindowState.Normal;
-            mainWindow.Show();
-            mainWindow.Activate();
-        }
-
-        private void startBreak(Object source, System.Timers.ElapsedEventArgs e)
-        {
-            System.Windows.Application.Current.Dispatcher.Invoke(() =>
+            Current.Dispatcher.Invoke(() =>
             {
                 LockScreen lockWindow = new LockScreen();
                 lockWindow.Show();
             });
         }
 
-        private void NotifyIconOnClickedOpen(object sender, EventArgs e)
+        private void NotifyIcon_OpenApp(object? sender, EventArgs e)
         {
-            NotifyIcon_DoubleClick(sender, e);
-        }
-        private void NotifyIconOnClickedClose(object sender, EventArgs e)
-        {
-            Current.Shutdown();
+            _mainWindow ??= new MainWindow();
+            _mainWindow.WindowState = System.Windows.WindowState.Normal;
+            _mainWindow.Show();
+            _mainWindow.Activate();
         }
 
-        private void ReviveAgentOfLazyUp(bool reviveProgram)
+        private void NotifyIcon_DoubleClick(object? sender, EventArgs e)
         {
+            NotifyIcon_OpenApp(sender, e);
+        }
 
+        private void NotifyIconOnClickedOpen(object? sender, EventArgs e)
+        {
+            NotifyIcon_OpenApp(sender, e);
+        }
+
+        private void NotifyIconOnClickedClose(object? sender, EventArgs e)
+        {
+            /*Current.Shutdown();*/
+            Environment.Exit(0);
+        }
+
+        static private void ReviveAgentOfLazyUp(bool reviveProgram, out Thread thread)
+        {
             // Имя программы, которую нужно проверить
             string processName = "AgentOfLazyUp";
             CancellationTokenSource cts = new CancellationTokenSource();
-            Thread thread = new Thread(() => CheckAndStartProcess(cts.Token));
+            thread = new Thread(() => CheckAndStartProcess(cts.Token));
 
             if (reviveProgram)
             {
                 // Запускаем проверку в фоновом потоке
                 thread.Start();
-            }   else
+            }
+            else
             {
                 cts.Cancel(); // Останавливаем поток
 
@@ -159,7 +183,8 @@ namespace LazyUp
                 // Если процесс найден, останавливаем его
                 if (foundProcesses.Length > 0)
                 {
-                    for (int i = 0; i < foundProcesses.Length; i++) { 
+                    for (int i = 0; i < foundProcesses.Length; i++)
+                    {
                         foundProcesses[i].Kill();
                     }
                 }
@@ -190,51 +215,142 @@ namespace LazyUp
             }
         }
 
-        private void Application_Startup(object sender, StartupEventArgs e)
+        static private void ReviveAgentOfLazyUp(bool reviveProgram)
         {
-            _notifyIcon = new Forms.NotifyIcon();
-            _notifyIcon.Icon = logoIcon;
+            // Имя программы, которую нужно проверить
+            string processName = "AgentOfLazyUp";
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Thread thread = new Thread(() => CheckAndStartProcess(cts.Token));
 
-            timerBreakInterval = new System.Timers.Timer();
-            timerBreakInterval.Interval = (config.BreaksIntervalSec + config.DurationBreakSec) * 1000;
-            timerBreakInterval.Elapsed += startBreak;
+            if (reviveProgram)
+            {
+                // Запускаем проверку в фоновом потоке
+                thread.Start();
+            }
+            else
+            {
+                cts.Cancel(); // Останавливаем поток
+
+                // Ищем процесс по имени
+                Process[] foundProcesses = Process.GetProcessesByName(processName);
+
+                // Если процесс найден, останавливаем его
+                if (foundProcesses.Length > 0)
+                {
+                    for (int i = 0; i < foundProcesses.Length; i++)
+                    {
+                        foundProcesses[i].Kill();
+                    }
+                }
+            }
+
+            void CheckAndStartProcess(CancellationToken token)
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Process[] foundProcesses = Process.GetProcessesByName(processName);
+                    // Проверяем, запущена ли программа
+                    if (foundProcesses.Length < 1)
+                    {
+                        // Программа не запущена, запускаем ее
+                        Process process = Process.Start(processName + ".exe");
+                        process.WaitForExit();
+                    } /*else if (foundProcesses.Length > 1)
+                    {
+                        for (int i = 0; i < foundProcesses.Length; i++)
+                        {
+                            foundProcesses[i].Kill();
+                        }
+                    }*/
+
+                    // Ждем перед следующей проверкой
+                    Thread.Sleep(5000);
+                }
+            }
+        }
+
+        delegate void StartBreakDelegate(Object? source, System.Timers.ElapsedEventArgs e);
+        static private void SetUpBreaksTimer(ref System.Timers.Timer timerBreakInterval, int intervalSec, int durationSec, StartBreakDelegate startBreak)
+        {
+            timerBreakInterval ??= new System.Timers.Timer();
+            timerBreakInterval.Interval = (intervalSec + durationSec) * 1000;
+            timerBreakInterval.Elapsed += new ElapsedEventHandler(startBreak);
             timerBreakInterval.AutoReset = true;
             timerBreakInterval.Enabled = true;
+        }
 
-            if (!config.StartInTray)
+        static private void SetUpStartingInTray(ref MainWindow mainWindow, bool StartInTray)
+        {
+            mainWindow ??= new MainWindow();
+            if (!StartInTray)
             {
-                mainWindow = mainWindow ?? new MainWindow();
                 mainWindow.WindowState = System.Windows.WindowState.Normal;
                 mainWindow.Show();
                 mainWindow.Activate();
             }
+        }
 
-            SetStartupProgram(config.StartupWithSystem);
+        private void SetUpConfigWatcher(ref FileSystemWatcher configWatcher)
+        {
+            string configFilePath = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "App._config");
+            configWatcher ??= new FileSystemWatcher(Path.GetDirectoryName(configFilePath) ?? @".\", Path.GetFileName(configFilePath));
+            configWatcher.NotifyFilter = NotifyFilters.LastWrite;
+            configWatcher.Changed += OnConfigChanged;
+            configWatcher.EnableRaisingEvents = true;
+        }
 
-            SetCloseInTray(config.CloseInTray);
+        private void SetUpNotifyIcon(ref Forms.NotifyIcon notifyIcon)
+        {
+            notifyIcon ??= new Forms.NotifyIcon();
+            notifyIcon.Text = _programBrandName;
+            notifyIcon.DoubleClick += NotifyIcon_DoubleClick;
+            notifyIcon.ContextMenuStrip = new Forms.ContextMenuStrip();
+        }
+        private void SetUpNotifyIcon(ref Forms.NotifyIcon notifyIcon, ref Dictionary<string, System.Drawing.Icon> icons)
+        {
+            notifyIcon ??= new Forms.NotifyIcon();
+            notifyIcon.Text = _programBrandName;
+            notifyIcon.DoubleClick += NotifyIcon_DoubleClick;
+            notifyIcon.ContextMenuStrip = new Forms.ContextMenuStrip();
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                notifyIcon.Icon = icons["logoIcon"];
+                notifyIcon.ContextMenuStrip.Items.Add("Open", icons["openIcon"].ToBitmap(), NotifyIconOnClickedOpen);
+                notifyIcon.ContextMenuStrip.Items.Add("Close", icons["closeIcon"].ToBitmap(), NotifyIconOnClickedClose);
+            }
+        }
 
-            string configFilePath = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "App.config");
-            _configWatcher = new FileSystemWatcher(Path.GetDirectoryName(configFilePath), Path.GetFileName(configFilePath));
-            _configWatcher.NotifyFilter = NotifyFilters.LastWrite;
-            _configWatcher.Changed += OnConfigChanged;
-            _configWatcher.EnableRaisingEvents = true;
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            SetStartupProgram(_config.StartupWithSystem);
+            SetUpStartingInTray(ref _mainWindow, _config.StartInTray);
 
-            _notifyIcon.Text = programBrandName;
-            _notifyIcon.DoubleClick += NotifyIcon_DoubleClick;
-            _notifyIcon.ContextMenuStrip = new Forms.ContextMenuStrip();
-            _notifyIcon.ContextMenuStrip.Items.Add("Open", openIcon.ToBitmap(), NotifyIconOnClickedOpen);
-            _notifyIcon.ContextMenuStrip.Items.Add("Close", closeIcon.ToBitmap(), NotifyIconOnClickedClose);
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Dictionary<string, System.Drawing.Icon> iconsPack = new Dictionary<string, System.Drawing.Icon>
+                {
+                    { "logoIcon", _logoIcon },
+                    { "openIcon", _openIcon },
+                    { "closeIcon", _closeIcon }
+                };
+                SetUpNotifyIcon(ref _notifyIcon, ref iconsPack);
+            }
+            else
+            {
+                SetUpNotifyIcon(ref _notifyIcon);
+            }
+            SetProgramVisibility(ref _notifyIcon, _config.HideProgram);
 
-            SetProgramVisibility(config.HideProgram);
-
-            ReviveAgentOfLazyUp(config.ReviveProgram);
+            SetUpBreaksTimer(ref _timerBreakInterval, _config.BreaksIntervalSec, _config.DurationBreakSec, StartBreak);
+            SetCloseInTray(out _shutdownModeValue, _config.CloseInTray);
+            SetUpConfigWatcher(ref _configWatcher);
+            ReviveAgentOfLazyUp(_config.ReviveProgram);
         }
 
         private void Application_Exit(object sender, ExitEventArgs e)
         {
             _notifyIcon.Visible = false;
             _notifyIcon.Dispose();
-            _notifyIcon = null;
         }
     }
 }
